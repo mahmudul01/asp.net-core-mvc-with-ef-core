75795b554419b144c70e5508a33d62348949633b
smhas <smhas@DESKTOP-NMQOVGI>
2020-11-14 12:53:12 +0600

    ASP.NET Core MVC with EF Core simple project.

---
+++ ContosoUniversity/Migrations/20201005072605_Add_MaxLengthOfName_andColumnName.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class Add_MaxLengthOfName_andColumnName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Student",
+                newName: "FirstName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Student",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Student",
+                newName: "FirstMidName");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstMidName",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
